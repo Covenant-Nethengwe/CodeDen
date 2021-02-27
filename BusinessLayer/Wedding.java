463cd62c9904baf3c9cd2338df4280b074381e68
AmandaNdukula <mandyndukula@gmail.com>
2021-02-27 18:16:52 +0200

    OverrideMethodsFromEventsOntoGraduationWedding

---
+++ BusinessLayer/Wedding.java
@@ -0,0 +1,27 @@
+import BusinessLayer.Event;
+
+public class Wedding extends Event{
+
+    @Override
+    public int registerEvent() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Boolean booking(int bookingId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Event confirmedBooking(Boolean booking) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double calculateTotal(Event typeOfEvent) {
+        // TODO Auto-generated method stub
+        return 0;
+    }}
