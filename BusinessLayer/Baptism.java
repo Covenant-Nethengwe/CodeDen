193541e29ef5e702887a7020f164adaa19a05ba8
Thapelo Tlhako <576525@student.belgiumcampus.ac.za>
2021-02-27 14:25:37 +0200

    Overriding the methods in Baptism and Party Class

---
+++ BusinessLayer/Baptism.java
@@ -0,0 +1,29 @@
+package BusinessLayer;
+
+public class Baptism extends Event{
+
+    @Override
+    public int registerEvent() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Boolean booking(int bookingId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Event confirmedBooking(Boolean booking) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double calculateTotal(Event typeOfEvent) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
--- BusinessLayer/Party.java
+++ BusinessLayer/Party.java
@@ -1,5 +1,31 @@
 package BusinessLayer;
 
-public class Party {
+import BusinessLayer.Event;
+
+public class Party extends Event {
+
+    @Override
+    public int registerEvent() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Boolean booking(int bookingId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Event confirmedBooking(Boolean booking) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double calculateTotal(Event typeOfEvent) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
 }
